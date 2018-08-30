5819bbaf1d49fd94023f4e155d57cf5919086b02
Trevor Cardoza <trevor.cardoza1538@gmail.com>
2018-08-30 07:50:43 -0700

    Chaos Modification
    School work where I created a fail function for a value higher the 1 and lower then 0

---
+++ Better chaos.py
@@ -0,0 +1,20 @@
+# File: chaos.py
+# A simple program illustrating chaotic behavior.
+
+def main():
+    print("This program illustrates a chaotic function")
+    x = eval(input("Enter a number between 0 and 1: "))
+    if x <= 1:
+        for i in range(10):
+            x = 3.9 * x * (1 - x)
+            print(x)
+    if x >= 0:
+         for i in range(10):
+            x = 3.9 * x * (1 - x)
+            print(x)
+            main()
+    else:
+        print("try again")
+        main()
+
+main()
