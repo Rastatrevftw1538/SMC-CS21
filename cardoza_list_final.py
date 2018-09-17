d112658b956bd440300f0f062b9e320e2a62b679
Trevor Cardoza <trevor.cardoza1538@gmail.com>
2018-09-17 09:48:32 -0700

    20 min in class assignment

---
+++ cardoza_list_final.py
@@ -0,0 +1,31 @@
+# Trevor Cardoza
+# CS 21, Fall 2018
+# Program: lists
+
+"""
+    Module description:
+    
+    File that sorts and reverses numbers.
+"""
+
+"""
+    Description of reverse function:
+
+    grabs numbers in list, sorts then spits in decreasing order.
+
+"""
+def reverse(my_list, if_greater=False):
+    for i in range(0,9):
+        if not isinstance(my_list, list):
+            return None
+        if if_greater != True:
+            my_list.sort()
+            my_list.reverse()
+        else:
+            return None
+    return my_list
+def main():
+    new_list = reverse([1,3,6,2,1,7])
+    print (new_list)
+    
+main()
